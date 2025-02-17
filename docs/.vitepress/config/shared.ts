2450a1df6eb15151b5ef10615663a52f1ec248dc
cynber <26402139+cynber@users.noreply.github.com>
2025-02-17 09:55:47 -0800

    attempt fix of indexing issue

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -20,9 +20,20 @@ export const shared = defineConfig({
 
     head: [
         ['link', { rel: 'icon', href: '/favicon-bg.svg' }],
-        ['meta', { property: 'og:image', content: 'https://fedecan.ca/img/social-share.png' }]
+        ['meta', { property: 'og:image', content: 'https://fedecan.ca/img/social-share.png' }],
+        ['meta', { name: 'robots', content: 'index,follow' }]
     ],
-
+    transformPageData(pageData) {
+        const canonicalUrl = `https://fedecan.ca${pageData.relativePath}`
+            .replace(/index\.md$/, '')
+            .replace(/\.md$/, '')
+        
+        pageData.frontmatter.head ??= []
+        pageData.frontmatter.head.push([
+            'link',
+            { rel: 'canonical', href: canonicalUrl }
+        ])
+    },
     themeConfig: {
         logo: {
             light: '/img/icons/maple-leaf.svg',
