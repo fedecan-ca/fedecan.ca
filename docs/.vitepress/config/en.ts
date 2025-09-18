38d02bbeda8e11707dbf3c3377e3cc8ec42913d0
Rooki <34943569+Pdzly@users.noreply.github.com>
2025-09-18 14:04:34 +0200

    Refactor Lemmy automation docs and update related links (#52)
    Consolidated and standardized Lemmy automation content under a unified path (`/en/guide/lemmy/automation`) for better clarity and navigation. Adjusted links, formatting, and added a new card to user guides for improved accessibility.
    
    Co-authored-by: cynber <26402139+cynber@users.noreply.github.com>

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from 'vitepress'
-import type { DefaultTheme } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 export const en = defineConfig({
     lang: 'en-CA',
@@ -9,11 +8,11 @@ export const en = defineConfig({
 
     themeConfig: {
         nav: [
-            { text: 'Home', link: '/en/' },
-            { text: 'Guides & Resources', link: '/en/guide/get-started' },
-            { text: 'Announcements', link: '/en/announcements' },
-            { text: 'Contact', link: '/en/contact' },
-            { text: '❤️ Donate', link: '/en/donate' },
+            {text: 'Home', link: '/en/'},
+            {text: 'Guides & Resources', link: '/en/guide/get-started'},
+            {text: 'Announcements', link: '/en/announcements'},
+            {text: 'Contact', link: '/en/contact'},
+            {text: '❤️ Donate', link: '/en/donate'},
         ],
 
         sidebar: {
@@ -21,13 +20,13 @@ export const en = defineConfig({
                 {
                     text: 'Getting Started',
                     items: [
-                        { text: 'What is the Fediverse?', link: '/en/guide/get-started' },
+                        {text: 'What is the Fediverse?', link: '/en/guide/get-started'},
                         {
                             text: 'Fedecan',
                             items: [
-                                { text: 'Who We Are', link: '/en/guide/fedecan' },
-                                { text: 'Our Platforms', link: '/en/guide/fedecan/our-platforms' },
-                                { text: 'F.A.Q.', link: '/en/guide/fedecan/faq' }
+                                {text: 'Who We Are', link: '/en/guide/fedecan'},
+                                {text: 'Our Platforms', link: '/en/guide/fedecan/our-platforms'},
+                                {text: 'F.A.Q.', link: '/en/guide/fedecan/faq'}
                             ]
                         }
                     ]
@@ -123,22 +122,22 @@ export const en = defineConfig({
                                                 }
                                             ]
                                         },
-                                        {
-                                            text: 'Automation for Lemmy',
-                                            link: '/en/guide/lemmy/for-admins/automation'
-                                        },
                                     ]
                                 },
                                 // {
                                 //     text: 'Guides for Developers',
                                 //     link: '/en/guide/lemmy/for-developers'
                                 // }
+                                {
+                                    text: 'Automation for Lemmy',
+                                    link: '/en/guide/lemmy/automation'
+                                },
                             ]
                         },
                         {
                             text: 'Pixelfed',
                             items: [
-                                { text: 'Overview', link: '/en/guide/pixelfed/overview' },
+                                {text: 'Overview', link: '/en/guide/pixelfed/overview'},
                             ]
                         },
                         {
