018ff2c759ce43030ab282c5732458be02363e87
Cynber <26402139+cynber@users.noreply.github.com>
2026-05-12 01:51:33 -0700

    fix footer scaling issues

--- docs/.vitepress/theme/CustomLayout.vue
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -2,15 +2,30 @@
 import DefaultTheme from 'vitepress/theme'
 import CustomFooter from './components/CustomFooter.vue'
 import CustomNotFound from './components/CustomNotFound.vue'
+import { useData } from 'vitepress'
+import { computed } from 'vue'
 
 const { Layout } = DefaultTheme
+const { frontmatter } = useData()
+
+// doc layout is the default (no layout set, or layout: 'doc')
+const isDoc = computed(
+  () => !frontmatter.value.layout || frontmatter.value.layout === 'doc'
+)
 </script>
 
 <template>
   <Layout>
-    <template #layout-bottom>
+    <!-- Doc layout: this slot is safely inside the content column, aside never overlaps -->
+    <template v-if="isDoc" #doc-bottom>
       <CustomFooter />
     </template>
+
+    <!-- Home + page + anything else: use layout-bottom, card styles handle centering -->
+    <template v-if="!isDoc" #layout-bottom>
+      <CustomFooter />
+    </template>
+
     <template #not-found>
       <CustomNotFound />
     </template>
