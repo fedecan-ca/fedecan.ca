4685e69570f058b34f1fdc91b057705e07fd7692
Cynber <26402139+cynber@users.noreply.github.com>
2026-05-14 02:48:04 -0700

    upload pending announcements

--- docs/.vitepress/theme/data/authors_en.js
+++ docs/.vitepress/theme/data/authors_en.js
@@ -2,19 +2,19 @@ const enAuthors = {
   fedecan: {
     name: 'Fedecan',
     avatar: '/announcements/author/profile-square-light.png',
-    // description: 'Non-Profit',
+    description: 'Organization',
     url: '..',
   },
   shadow: {
     name: 'Shadow',
     avatar: '/announcements/author/shadow.webp',
-    description: 'lemmy.ca Admin',
+    description: 'Admin',
     url: 'https://lemmy.ca/u/Shadow',
   },
   otter: {
     name: 'Otter',
     avatar: '/announcements/author/otter.webp',
-    description: 'lemmy.ca Admin',
+    description: 'Admin',
     url: 'https://lemmy.ca/u/otter',
   },
 }
--- docs/.vitepress/theme/data/authors_fr.js
+++ docs/.vitepress/theme/data/authors_fr.js
@@ -2,19 +2,19 @@ const frAuthors = {
   fedecan: {
     name: 'Fédécan',
     avatar: '/announcements/author/profile-square-light.png',
-    // description: 'Non-Profit',
+    description: 'Organisation',
     url: '..',
   },
   shadow: {
     name: 'Shadow',
     avatar: '/announcements/author/shadow.webp',
-    description: 'lemmy.ca Administrateur',
+    description: 'Administrateur',
     url: 'https://lemmy.ca/u/Shadow',
   },
   otter: {
     name: 'otter',
     avatar: '/announcements/author/otter.webp',
-    description: 'lemmy.ca Administrateur',
+    description: 'Administrateur',
     url: 'https://lemmy.ca/u/otter',
   },
 }
